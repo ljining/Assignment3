d6f65925373afe6eb43a97f1a1fc0d8e47f88193
ljining <108book@naver.com>
2024-03-27 18:00:50 +0900

    1

---
+++ ToDo2/TableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  TableViewCell.swift
+//  ToDo2
+//
+//  Created by 이유진 on 3/27/24.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    @IBAction func textField(_ sender: UITextField) {
+    }
+    
+    @IBAction func checkButton(_ sender: UIButton) {
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
--- ToDo2/ViewController.swift
+++ ToDo2/ViewController.swift
@@ -7,13 +7,50 @@
 
 import UIKit
 
+
+
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    var tasks = [Task]()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
     }
 
 
+}
+
+struct Task {
+    let id: Int
+    var title: String
+    var isCompleted: Bool
+    
+    init(id: Int, title: String, isCompleted: Bool) {
+        self.id = id
+        self.title = title
+        self.isCompleted = isCompleted
+    }
+}
+
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    //tableView에 할 일 목록 개수 반환(행)
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tasks.count
+    }
+    
+    //tableView 각 행의 셀 반환. 텍스트 필드 활성화 및 텍스트 설정
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TableViewCell", for: indexPath) as! TableViewCell
+        
+        let textField = cell.contentView.subviews.compactMap { $0 as? UITextField }.first
+        textField?.isUserInteractionEnabled = tableView.isEditing
+        textField?.text = tasks[indexPath.row].title
+        
+        return cell
+    }
 }
 
