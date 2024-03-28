f6214cfcfec50db69426663e601f0858d936d367
ljining <108book@naver.com>
2024-03-28 20:54:15 +0900

    완료

--- ToDo2/TableViewCell.swift
+++ ToDo2/TableViewCell.swift
@@ -7,23 +7,41 @@
 
 import UIKit
 
-class TableViewCell: UITableViewCell {
+class TableViewCell: UITableViewCell, UITextFieldDelegate {
 
     @IBOutlet weak var todoList: UITextField!
     @IBOutlet weak var tappedButton: UIButton!
     
     @IBAction func checked(_ sender: UIButton) {
         sender.isSelected = !sender.isSelected
+        updateTextFieldStyle()
     }
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        
+        updateTextFieldStyle()
+        todoList.delegate = self // UITextFieldDelegate 설정
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
+        updateTextFieldStyle()
     }
+
+    func updateTextFieldStyle() {
+        let attributeString: NSMutableAttributedString =  NSMutableAttributedString(string: todoList.text ?? "")
+        if tappedButton.isSelected {
+            // 체크박스가 선택된 경우 중간 줄을 추가합니다.
+            attributeString.addAttribute(NSAttributedString.Key.strikethroughStyle, value: 2, range: NSMakeRange(0, attributeString.length))
+        } else {
+            // 체크박스가 선택되지 않은 경우 중간 줄을 제거합니다.
+            attributeString.removeAttribute(NSAttributedString.Key.strikethroughStyle, range: NSMakeRange(0, attributeString.length))
+        }
+        todoList.attributedText = attributeString
+        }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+           tappedButton.isSelected = false // 체크박스의 isSelected 속성을 false로 설정하여 해제
+       }
     
 }
--- ToDo2/ViewController.swift
+++ ToDo2/ViewController.swift
@@ -13,6 +13,55 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
     var tasks = [Task]()
+    var editedTasks = [Task]()
+    
+    
+    @IBAction func editButton(_ sender: UIBarButtonItem) {
+        tableView.setEditing(!tableView.isEditing, animated: true)
+        
+        let buttonText = tableView.isEditing ? "Done" : "Edit"
+            navigationItem.leftBarButtonItem = UIBarButtonItem(title: buttonText, style: .plain, target: self, action: #selector(editButton(_:)))
+        
+        for indexPath in tableView.indexPathsForVisibleRows ?? [] {
+            if let cell = tableView.cellForRow(at: indexPath) as? TableViewCell {
+            cell.todoList.isUserInteractionEnabled = tableView.isEditing
+            }
+        }
+        
+        if !tableView.isEditing { // Done 버튼을 눌렀을 때
+                // 수정된 값으로 기존 tasks 배열 업데이트
+                for indexPath in tableView.indexPathsForVisibleRows ?? [] {
+                    if let cell = tableView.cellForRow(at: indexPath) as? TableViewCell,
+                       let newText = cell.todoList.text, !newText.isEmpty {
+                        tasks[indexPath.row].title = newText
+                    }
+                }
+            }
+    }
+    
+    @IBAction func addButton(_ sender: UIBarButtonItem) {
+        let alertControl = UIAlertController(title: "할 일 추가", message: "", preferredStyle: .alert)
+                
+                let cancelButton = UIAlertAction(title: "취소", style: .default, handler: nil)
+                alertControl.addAction(cancelButton)
+                
+                let addButton = UIAlertAction(title: "추가", style: .default) { _ in
+                    if let textField = alertControl.textFields?.first,
+                       let text = textField.text {
+                        let newTask = Task(id: self.tasks.count + 1, title: text, isCompleted: false)
+                        self.tasks.append(newTask)
+                        self.tableView.reloadData()
+                    }
+                }
+                alertControl.addAction(addButton)
+                
+                alertControl.addTextField { textField in
+                    textField.placeholder = "할 일을 입력하세요"
+                }
+                
+                self.present(alertControl, animated: true, completion: nil)
+            }
+    
     
     
     override func viewDidLoad() {
@@ -22,7 +71,6 @@ class ViewController: UIViewController {
         tableView.delegate = self
         
         tasks.append(Task(id: 1, title: "First", isCompleted: false))
-
     }
 
 
@@ -54,9 +102,31 @@ extension ViewController: UITableViewDataSource, UITableViewDelegate {
         let task = tasks[indexPath.row]
         cell.todoList.text = task.title
         cell.tappedButton.setTitle("Button", for: .normal)
-        cell.todoList.isUserInteractionEnabled = true
+        cell.todoList.isUserInteractionEnabled = tableView.isEditing
         
         return cell
     }
-}
+    
+    // 테이블뷰에서 특정 행이 선택되었을 때, 메서드 호출해 선택 상태 해제
+        func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+            tableView.deselectRow(at: indexPath, animated: true)
+        }
+    
+    func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
+        return .delete // 삭제를 허용하는 스타일
+    }
+    
+    // 삭제 처리
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // 삭제할 데이터를 제거합니다.
+            tasks.remove(at: indexPath.row)
+            
+            // 테이블 뷰에서 해당 행을 삭제합니다.
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        }
+    }
+    
+    
 
+}
